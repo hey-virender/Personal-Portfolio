098529169ca41ffade998e82855f22ed65b2b351
Virender Chauhan <bikuchauhan786+github@gmail.com>
2023-10-22 21:31:43 +0530

    bg changed improved js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function showDiv(divNumber) {
-    const divs = document.querySelectorAll('.content');
-    for (let i = 0; i < divs.length; i++) {
-        divs[i].classList.remove('visible');
+let currentOpenDiv = null;
+
+function showOrHideDiv(divNumber) {
+    const div = document.getElementById('div' + divNumber);
+
+    if (div === currentOpenDiv) {
+        // Clicked on the same button again, hide the div
+        div.classList.remove('visible');
+        document.getElementById('info').classList.remove('hidden')
+        currentOpenDiv = null;
+    } else {
+        // Clicked on a different button, hide the currently open div (if any)
+        if (currentOpenDiv) {
+            currentOpenDiv.classList.remove('visible');
+            document.getElementById('info').classList.remove('hidden')
+        }
+
+        // Show the selected div
+        div.classList.add('visible');
+        document.getElementById('info').classList.add('hidden')
+        currentOpenDiv = div;
     }
-    document.getElementById('div' + divNumber).classList.add('visible');
+}
+
+function hideinfo(){
+    
 }
