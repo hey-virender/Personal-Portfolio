25666db000ff9f44653394d927a992c7a8fde4c6
Virender Chauhan <bikuchauhan786+github@gmail.com>
2023-10-23 15:41:50 +0530

    js enhanced

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-/** 
-let currentOpenDiv = null;
-let currentLabel = null;
 
-function showOrHideDiv(divNumber,labelNo) {
-    const div = document.getElementById('div' + divNumber);
-    const label = document.getElementById('l' + labelNo);
-
-    if (div === currentOpenDiv && label === currentLabel) {
-        // Clicked on the same button again, hide the div
-        div.classList.remove('visible');
-        label.classList.remove('active');
-        document.getElementById('info').classList.remove('hidden');
-        currentOpenDiv = null;
-        currentLabel = null;
-    } else {
-        // Clicked on a different button, hide the currently open div (if any)
-        if (currentOpenDiv && currentLabel) {
-            currentOpenDiv.classList.remove('visible');
-            label.classList.remove('active');
-            document.getElementById('info').classList.remove('hidden');
-        }
-
-        // Show the selected div
-        div.classList.add('visible');
-        label.classList.add('active');
-        document.getElementById('info').classList.add('hidden')
-        currentOpenDiv = div;
-        currentLabel = label;
-    }
-}*/
 let currentOpenDiv = null;
 let currentLabel = null;
 
