5dfda66a5a188ff62e0bd4091fbf1e8acc03eb68
Virender Chauhan <bikuchauhan786+github@gmail.com>
2023-10-23 15:38:38 +0530

    updated js to show currently open div

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
+/** 
 let currentOpenDiv = null;
+let currentLabel = null;
 
-function showOrHideDiv(divNumber) {
+function showOrHideDiv(divNumber,labelNo) {
     const div = document.getElementById('div' + divNumber);
+    const label = document.getElementById('l' + labelNo);
 
-    if (div === currentOpenDiv) {
+    if (div === currentOpenDiv && label === currentLabel) {
         // Clicked on the same button again, hide the div
         div.classList.remove('visible');
-        document.getElementById('info').classList.remove('hidden')
+        label.classList.remove('active');
+        document.getElementById('info').classList.remove('hidden');
         currentOpenDiv = null;
+        currentLabel = null;
     } else {
         // Clicked on a different button, hide the currently open div (if any)
-        if (currentOpenDiv) {
+        if (currentOpenDiv && currentLabel) {
             currentOpenDiv.classList.remove('visible');
-            document.getElementById('info').classList.remove('hidden')
+            label.classList.remove('active');
+            document.getElementById('info').classList.remove('hidden');
         }
 
         // Show the selected div
         div.classList.add('visible');
+        label.classList.add('active');
         document.getElementById('info').classList.add('hidden')
         currentOpenDiv = div;
+        currentLabel = label;
+    }
+}*/
+let currentOpenDiv = null;
+let currentLabel = null;
+
+function showOrHideDiv(divNumber, labelNo) {
+    const div = document.getElementById('div' + divNumber);
+    const label = document.getElementById('l' + labelNo);
+
+    if (div === currentOpenDiv && label === currentLabel) {
+        // Clicked on the same button again, hide the div
+        div.classList.remove('visible');
+        label.classList.remove('active');
+        document.getElementById('info').classList.remove('hidden');
+        currentOpenDiv = null;
+        currentLabel = null;
+    } else {
+        // Clicked on a different button, hide the currently open div (if any)
+        if (currentOpenDiv && currentLabel) {
+            currentOpenDiv.classList.remove('visible');
+            currentLabel.classList.remove('active');
+            document.getElementById('info').classList.remove('hidden');
+        }
+
+        // Show the selected div
+        div.classList.add('visible');
+        label.classList.add('active');
+        document.getElementById('info').classList.add('hidden');
+        currentOpenDiv = div;
+        currentLabel = label;
     }
 }
-
-function hideinfo(){
-    
-}
+ 
